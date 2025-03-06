9d7bdcf4f740d5a72db3b9353bafc0c84cb2af62
Dennis Hurst <dennishurst@users.noreply.github.com>
2025-03-06 12:10:01 -0500

    Adding python sample code

---
+++ code_sample.py
@@ -0,0 +1,12 @@
+#*
+#* Copyright (c) 2025 Saltworks Security
+#*
+#* Use of this software is governed by the Business Source License included
+#* in the LICENSE.TXT file.
+#*
+#* Change Date: 2029-01-28
+#*
+#* On the date above, in accordance with the Business Source License, use
+#* of this software will be governed by version 2 or later of the General
+#* Public License.
+#*
